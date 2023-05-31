dc4976d136b70da135a541f0972463ead1d26078
Taras Yemets <yemets.taras@gmail.com>
2023-05-31 12:35:56 +0300

    upd log message

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ function main() {
   const { port1, port2 } = new MessageChannel();
   port1.start();
   port1.addEventListener("message", (event) => {
-    console.log("Incoming message on port1", event.data);
+    console.log("Incoming message on port1:", event.data);
   });
   const { port1: anotherPort1, port2: anotherPort2 } = new MessageChannel();
   port2.postMessage({ value: anotherPort1 }, [anotherPort1]);
